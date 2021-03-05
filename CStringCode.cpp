477bcae25e7ef75b25161a4c65c0ffa86be7f933
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2021-03-04 16:01:12 -0800

    first commit

---
+++ CStringCode.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int vowelCount(char * str);
+int consonantCount(char * str);
+bool isVowel(char ch);
+bool isConsonant(char ch);
+bool isLetter(char ch);
+bool verifyPassword(char * str);
+
+//Note:
+/*
+ Functions 1, 2 and 3 in the requirements document are already done for you. Do 4, 5, and 6.
+ Make Seperate issues for each function on GitHub, and then at the end, add your header, and any other
+ neccessary documentation comments.
+
+ DO NOT TOUCH Functions 4 5 and 6, as they are already done for you.
+
+ When done with assignment, make sure to run thorough testing before submission to Canvas.
+*/
+
+int main() {
+
+	char str[12]{ "bookkeeper" };
+	cout << "The number of vowels in " << str << " is " << vowelCount(str) << endl;
+
+	system("pause");
+	return 0;
+}
+
+int vowelCount(char * str) {
+	int vowelCount = 0;
+	// your code here
+	return vowelCount;
+}
+
+
+bool isVowel(char ch) {
+	return ch == 'a' || ch == 'e' || ch == 'i' ||
+		ch == 'o' || ch == 'u' || ch == 'A' ||
+		ch == 'E' || ch == 'I' || ch == 'O' ||
+		ch == 'U';
+}
+
+bool isConsonant(char ch) {
+	return isLetter(ch) && !isVowel(ch);
+}
+
+bool isLetter(char ch) {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z';
+}
+
+int consonantCount(char * str) {
+	// your code here
+	return 0;
+}
+
+bool verifyPassword(char * str) {
+	// your code here
+	return false;
+}
