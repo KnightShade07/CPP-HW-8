a7cbe7189f0adb82632e5379b7d0beb85929e7ee
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2021-03-04 20:46:26 -0800

    Added Documentation

--- CStringCode.cpp
+++ CStringCode.cpp
@@ -11,6 +11,13 @@ bool isConsonant(char ch);
 bool isLetter(char ch);
 bool verifyPassword(char * str);
 
+//Programming Assignment 8: C-Style Strings with Pointers
+//Ethen Oliva
+//Instructor: Ken Meerdink
+/*
+	Instructions: Follow the PDF and fill in the functions to produce the correct results using pointers.
+*/
+
 //Note:
 /*
  Functions 1, 2 and 3 in the requirements document are already done for you. Do 4, 5, and 6.
@@ -20,6 +27,19 @@ bool verifyPassword(char * str);
  DO NOT TOUCH Functions 4 5 and 6, as they are already done for you.
 
  When done with assignment, make sure to run thorough testing before submission to Canvas.
+ Final Tests Issue:
+ https://github.com/KnightShade07/CPP-HW-8/issues/4
+
+ //Note: Looking at the PDF for the assignment, the main function seems to have a typo.
+ "bookkeeper" should have instead been "bookkeepers"
+
+ To quote the PDF itself:
+
+ "Consonant counter: write a function named consonantCount that takes a C-style
+string (character array with null terminator) as a parameter and returns the number of
+consonants (any letter except a, e, i, o, and u) that occur in the string.
+For example, if we had char word[12]{"bookkeepers"}, then
+consonantCount(word) would return 6."
 */
 
 int main() {
@@ -79,7 +99,7 @@ int consonantCount(char * str) {
 		
 	}
 	//this returns 5, but the document says it should return 6. There is an s missing in the 
-	//char str variable, but I am not going to edit that.
+	//char str variable, but I am not going to edit that, see top of program for more details.
 	return consonantCount;
 }
 
@@ -110,7 +130,7 @@ bool verifyPassword(char * str) {
 		}
 		if (verifyCount == 3)
 		{
-			//returns a one on test, you're good!
+			//returns a one (true) on test, you're good!
 			return true;
 		}
 					
